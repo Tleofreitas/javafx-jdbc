51447199afea8317df634f569ad47a7435e7ef4b
Thiago <thiagoleonardo1995@live.com>
2024-12-23 10:22:16 -0300

    About View

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,7 +1,17 @@
 package gui;
 
+import java.io.IOException;
+
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController {
 	
@@ -23,6 +33,39 @@ public class MainViewController {
 	}
 	
 	public void onMenuItemSobreAction() {
-		System.out.println("onMenuItemSobreAction");
+		loadView("/gui/About.fxml");
+	}
+	
+	// synchronized para que o processamento todo não seja interrompido durante as multi-threads
+	private synchronized void loadView(String absoluteName) {
+		try {
+			// Carregar outra View
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();		
+			
+			// Pegar a referência da cena principal
+			Scene mainScene = Main.getMainScene();
+			
+			// Pegar o primeiro elemento da view principal, acessar o content e 
+			// pegar a referência para o VBox
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			// Pegar o primeiro filho da janela principal, o main menu
+			Node mainMenu = mainVBox.getChildren().get(0);
+			
+			// Limpar todos os filhos do main vbox
+			mainVBox.getChildren().clear();
+			
+			// Adicionar o main menu
+			mainVBox.getChildren().add(mainMenu);
+			
+			// Adicionar os filhos do new vbox (Sobre)
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+			
+			
+		} catch (IOException e) {
+			Alerts.showAlert("IOException", "Erro ao carregara a página", e.getMessage(), AlertType.ERROR);
+		}
 	}
 }
